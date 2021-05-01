1f318ef28a53f60425a7b2f79c0944369ebdcfec
DEXTERFREYGGANG <>
2021-05-01 19:50:40 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,29 @@
+class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+  }
+}
+
+
+
+
+
+
+/*
+
+https://docs.google.com/document/d/1gvv3NEF8Da0e9OxaioAIK80jBCy_ViHW8mR9KUC4kK0/edit
+
+//REQUISITOS PARTE A
+ - Dos niveles de anidacion:		Tenemos 6 niveles (universidad->escuela->carrera->asignatura->alumno->nota)
+ - Dos clases con sobrecarga:		Alumno (registrar->rut), Docente (registrar->rut)     //(si llega a hacer falta otra, lo hacemos con el promedio)
+ - Todos los atr. priv. con GyS:	Listo! (FALTA los GyS de los arreglos)
+ - Menu interactivo:				FALTA TODA LA PARTE LOGICA
+	- Insercion:					escuela, carrera, alumno, asignatura, nota, docente
+	- Edicion:						escuela, carrera, alumno, asignatura, nota, docente
+	- Eliminacion					escuela, carrera, alumno, asignatura, nota, docente
+	- Generacion de reportes:		mostrarCarrerasEscuela, mostrarAsignaturasCarrera, mostrarAlumnosCarrera, mostrarAlumnosAsignatura, mostrarNotasAlumno
+	- Funcionalidades propias:		promedioNotasAsignatura, promedioNotasAlumno
+
+//Funcionalidades
+
+*/
